95655a51fedd66043a898d78a06ff808d24861e5
mathew mclean <mathewjmclean@gmail.com>
2020-02-19 17:57:13 +0000

    requirements tweaks, spelling

--- pong/utils/subclasses.py
+++ pong/utils/subclasses.py
@@ -4,7 +4,7 @@ from kivy import vector
 
 
 class Vector(vector.Vector):
-    # Fixes lack of type hinting causeing issues in Pycharm
+    # Fixes lack of type hinting causing issues in Pycharm
     # noinspection PyBroadException
     def __mul__(self, val) -> Vector:
         try:
